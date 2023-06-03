eb37f460bccdbf8afec9a47a5d11e61fe289bb57
SonOfMosiah <dev@sonofmosiah.ai>
2023-06-02 20:14:31 -0600

    FIX: compile errors

--- src/AirdropERC1155.sol
+++ src/AirdropERC1155.sol
@@ -4,11 +4,10 @@ pragma solidity ^0.8.20;
 import "openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
 import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 import "openzeppelin-contracts/contracts/utils/Multicall.sol";
-import "openzeppelin-contracts/contracts/metatx/ERC2771Context.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
-import "src/interfaces/airdrop/IAirdropERC1155.sol";
+import "src/IAirdropERC1155.sol";
 
-contract AirdropERC1155 is Ownable, ReentrancyGuard, ERC2771Context, Multicall, IAirdropERC1155 {
+contract AirdropERC1155 is Ownable, ReentrancyGuard, Multicall, IAirdropERC1155 {
     error InvalidArrayLength();
 
     constructor() {}
@@ -19,7 +18,7 @@ contract AirdropERC1155 is Ownable, ReentrancyGuard, ERC2771Context, Multicall, 
      *                   which acts as operator for the tokens.
      *
      *  @param _tokenAddress    ERC1155 contract address
-     *  @param _tokenOwner      Address of the sender
+     *  @param _sender      Address of the sender
      *  @param _recipients      Airdrop recipients
      *  @param _amounts         Amount of tokens to airdrop to each recipient
      *  @param _tokenIds        List of ERC1155 tokenIds to airdrop
@@ -52,7 +51,7 @@ contract AirdropERC1155 is Ownable, ReentrancyGuard, ERC2771Context, Multicall, 
      *                   which acts as operator for the tokens.
      *
      *  @param _tokenAddress    ERC1155 contract address
-     *  @param _tokenOwner      Address of the sender
+     *  @param _sender      Address of the sender
      *  @param _recipients      Airdrop recipients
      *  @param _amounts         Amount of tokens to airdrop to each recipient
      *  @param _tokenId        TokenId to airdrop
@@ -64,15 +63,15 @@ contract AirdropERC1155 is Ownable, ReentrancyGuard, ERC2771Context, Multicall, 
         uint256[] memory _amounts,
         uint256 _tokenId
     ) external nonReentrant onlyOwner {
-        uint256 len = _tokenIds.length;
-        if (len == 0 || len != _recipients.length || len != _amounts.length) {
+        uint256 len = _recipients.length;
+        if (len == 0 || len != _amounts.length) {
             revert InvalidArrayLength();
         }
 
         IERC1155 token = IERC1155(_tokenAddress);
 
         for (uint256 i = 0; i < len;) {
-            token.safeTransferFrom(_sender, _recipients[i], tokenId, _amounts[i], "");
+            token.safeTransferFrom(_sender, _recipients[i], _tokenId, _amounts[i], "");
             unchecked {
                 i += 1;
             }
--- src/IAirdropERC1155.sol
+++ src/IAirdropERC1155.sol
@@ -1,14 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
-/**
- *  Thirdweb's `Airdrop` contracts provide a lightweight and easy to use mechanism
- *  to drop tokens.
- *
- *  `AirdropERC1155` contract is an airdrop contract for ERC1155 tokens. It follows a
- *  push mechanism for transfer of tokens to intended recipients.
- */
-
 interface IAirdropERC1155 {
     /**
      *  @notice          Lets contract-owner send ERC1155 tokens to a list of addresses.
@@ -16,19 +8,30 @@ interface IAirdropERC1155 {
      *                   which acts as operator for the tokens.
      *
      *  @param _tokenAddress    Contract address of ERC1155 tokens to air-drop.
-     *  @param _tokenOwner      Address from which to transfer tokens.
+     *  @param _sender      Address from which to transfer tokens.
      *  @param _recipients      List of recipient addresses for the air-drop.
      *  @param _amounts         Quantity of tokens to air-drop, per recipient.
      *  @param _tokenIds        List of ERC1155 token-Ids to drop.
      */
     function airdrop(
         address _tokenAddress,
-        address _tokenOwner,
+        address _sender,
         address[] memory _recipients,
         uint256[] memory _amounts,
         uint256[] memory _tokenIds
     ) external;
 
+    /**
+     *  @notice          Lets contract-owner send ERC1155 tokens to a list of addresses.
+     *  @dev             The sender should approve target tokens to Airdrop contract,
+     *                   which acts as operator for the tokens.
+     *
+     *  @param _tokenAddress    ERC1155 contract address
+     *  @param _sender      Address of the sender
+     *  @param _recipients      Airdrop recipients
+     *  @param _amounts         Amount of tokens to airdrop to each recipient
+     *  @param _tokenId        TokenId to airdrop
+     */
     function airdropSingle(
         address _tokenAddress,
         address _sender,
